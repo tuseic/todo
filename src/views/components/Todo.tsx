762ad27c97656438d23221c2e6639fb602ae957e
shimehituzi <shimehituzi@gmail.com>
2020-04-21 19:43:03 +0900

    form

--- src/views/components/Todo.tsx
+++ src/views/components/Todo.tsx
@@ -1,7 +1,49 @@
 import React from 'react'
+import { State } from 'state'
 
-export const Todo = () => {
+type Type = {
+  name: State['todo']['text']['name']
+  description: State['todo']['text']['description']
+}
+
+type OwnProps = {
+  name: Type['name']
+  description: Type['description']
+}
+
+type Handler = {
+  handleSetName: (
+    (name: Type['name']) => void
+  )
+  handleSetDescription: (
+    (description: Type['description']) => void
+  )
+}
+
+type Props = OwnProps & Handler
+
+export const Todo: React.FC<Props> = (props) => {
+
+  const setNameFunc = (e: React.ChangeEvent<HTMLInputElement>) => {
+    props.handleSetName(e.target.value)
+  }
+
+  const setDescriptionFunc = (e: React.ChangeEvent<HTMLInputElement>) => {
+    props.handleSetDescription(e.target.value)
+  }
+
   return (
-    <div>this is todo component</div>
+    <React.Fragment>
+      <input
+        onChange={setNameFunc}
+        value={props.name}
+        placeholder='name'
+      />
+      <input
+        onChange={setDescriptionFunc}
+        value={props.description}
+        placeholder='description'
+      />
+    </React.Fragment>
   )
 }
--- src/views/containers/TodoContainer.tsx
+++ src/views/containers/TodoContainer.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useCallback, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { State, initialState, actions } from 'state'
 import { Todo } from 'views/components/Todo'
 
+type Type = {
+  name: State['todo']['text']['name']
+  description: State['todo']['text']['description']
+}
+
 const TodoContainer = () => {
+  const name = useSelector<State, Type['name']>(
+    state => state.todo.text.name
+  )
+  const description = useSelector<State, Type['description']>(
+    state => state.todo.text.description
+  )
+
+  const dispatch = useDispatch()
+  const handleSetName = useCallback(
+    (name: Type['name']) => {
+      dispatch(actions.todo.setName(name))
+    }, [dispatch]
+  )
+  const handleSetDescription = useCallback(
+    (description: Type['description']) => {
+      dispatch(actions.todo.setDescription(description))
+    }, [dispatch]
+  )
+
+  useEffect(() => {
+    return () => {
+      handleSetName(initialState.todo.text.name)
+      handleSetDescription(initialState.todo.text.description)
+    }
+  }, [handleSetName, handleSetDescription])
+
+  const props = { name, description, handleSetName, handleSetDescription }
+
   return (
-    <Todo/>
+    <Todo {...props}/>
   )
 }
 
